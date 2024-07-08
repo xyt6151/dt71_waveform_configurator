21e6d26e72e14299865cc578bb59ab3948e7c0f4
xyt6151 <50144803+xyt6151@users.noreply.github.com>
2024-07-09 01:39:16 +1200

    Update start_venv.bat

--- start_venv.bat
+++ start_venv.bat
@@ -3,7 +3,7 @@ REM Change to the script directory
 cd /d "%~dp0"
 
 REM Activate the virtual environment
-call venv_old\Scripts\activate
+call venv\Scripts\activate
 
 REM Open a new command prompt window with the venv activated
 start cmd /k "call venv\Scripts\activate && echo Virtual environment activated. && cd /d %~dp0"
